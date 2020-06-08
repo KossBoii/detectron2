48e61f4c46bf743f5b35566845c227e7a11826e8
KossBoii <long.truong0320@gmail.com>
2020-06-08 14:55:59 -0700

    add visualize

--- projects/RoadStress/predict.py
+++ projects/RoadStress/predict.py
@@ -265,14 +265,58 @@ def main(args):
     return do_test(cfg, model)
     print("Done")
 
+def visualize():
+    for d in ["train", "val"]:
+        DatasetCatalog.register("roadstress_" + d, lambda d=d: get_roadstress_dicts("roadstress_new/" + d))
+        MetadataCatalog.get("roadstress_" + d).set(thing_classes=["roadstress"])
+
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.DATASETS.TRAIN = ("roadstress_train",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Transfer learning with weights from model_zoo
+    cfg.SOLVER.IMS_PER_BATCH = 1
+    cfg.SOLVER.BASE_LR = 0.005  # Learning rate
+    cfg.SOLVER.MAX_ITER = 20000 
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # Number of classification classes excluding the background - only has one class (roadstress)
+    cfg.MODEL.ANCHOR_GENERATOR.ANGLES = [[-120, -90, -30 , -45, -60, 0, 30, 45, 60, 90, 120]]
+    cfg.SOLVER.CHECKPOINT_PERIOD = 2000
+    cfg.MODEL.ANCHOR_GENERATOR.SIZES = [[8, 16, 32, 64, 128, 256, 512]]
+    cfg.TEST.DETECTIONS_PER_IMAGE = 1024
+
+    cfg.MODEL.WEIGHTS = "model_final.pth"
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.   # set the testing threshold for this model
+    cfg.DATASETS.TEST = ("roadstress_val", )
+    predictor = DefaultPredictor(cfg)
+
+    from detectron2.utils.visualizer import ColorMode
+    dataset_dicts = get_roadstress_dicts("roadstress_new/val")
+    for d in random.sample(dataset_dicts, 1):    
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)
+
+        v = Visualizer(im[:, :, ::-1],
+                    metadata=roadstress_metadata, 
+                    scale=1.0, 
+                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
+        )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        #cv2_imshow(v.get_image()[:, :, ::-1])
+        from skimage.io import imshow, imsave
+        img = v.get_image()[:, :, ::-1]
+        imsave("result.jpg", img)
+
 if __name__ == "__main__":
-    args = default_argument_parser().parse_args()
-    print("Command Line Args:", args)
-    launch(
-        main,
-        args.num_gpus,
-        num_machines=args.num_machines,
-        machine_rank=args.machine_rank,
-        dist_url=args.dist_url,
-        args=(args,),
-    )
+    visualize()
+    # args = default_argument_parser().parse_args()
+    # print("Command Line Args:", args)
+    # launch(
+    #     main,
+    #     args.num_gpus,
+    #     num_machines=args.num_machines,
+    #     machine_rank=args.machine_rank,
+    #     dist_url=args.dist_url,
+    #     args=(args,),
+    # )
